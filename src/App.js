674abfdf0537e111b5293aefa29f8c7368ea031a
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-09-26 21:06:14 -0500

    Added full course list populated by remote json

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
-
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]' }</Title>
 );
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
@@ -55,11 +29,26 @@ const CourseList = ({ courses }) => (
   </Button.Group>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 export default App;
